6f1a2be3ca503424a5f8130f28347bdb00d68909
Anthony Becerra <abece005@fiu.edu>
2017-09-27 14:17:53 -0400

    Finished all tasks

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -15,6 +15,23 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        public IActionResult Results(string searchType, string searchTerm) // searchType & searchTerm taken from Views/Search/Index.cshtml form.
+        {
+            ViewBag.columns = ListController.columnChoices;
 
+            // if user enters searchTerm, find jobs by searchTerm and pass them into the Views/Search/Index.cshtml view.
+            if (searchTerm != null)
+            {
+                ViewBag.jobs = JobData.FindByValue(searchTerm);
+                ViewBag.searchTerm = searchTerm;
+                return View("Index");
+            }
+
+            // else return back to SearchController > Index action method.
+            else
+            {
+                return Redirect("/search");
+            }
+        }
     }
 }
